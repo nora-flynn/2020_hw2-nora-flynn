6d7bf51b0a64b9526cef303d75c8a85a75273c48
nora-flynn <nflyn002@ucr.edu>
2020-11-05 19:04:04 -0800

    count_up corrected

--- count_up.py
+++ count_up.py
@@ -5,8 +5,8 @@ gfffile = os.path.basename(gffurl)
 print("1A: What is the GFF filename?")
 print("Gff filename is {}".format(gfffile))
 print("-----")
-if not os.path.exists(gfffile):
-    os.system("curl -O {}". format(gffurl))
+if not os.path.exists(gffurl):
+    os.system("curl -O {}ftp://ftp.ensemblgenomes.org/pub/bacteria/release-45/gff3/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.37.gff3.gz")
 
 
 fastaurl="Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.chromosome.Chromosome.fa.gz"
@@ -14,8 +14,8 @@ fastafile= os.path.basename(fastaurl)
 print("1B: What is the FASTA filename?")
 print("Fasta filename is {}". format(fastafile))
 print("-----")
-if not os.path.exists(fastafile):
-    os.system("curl -O {}".format(fastaurl))
+if not os.path.exists(fastaurl):
+    os.system("curl -O ftp://ftp.ensemblgenomes.org/pub/bacteria/release-45/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/dna/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.chromosome.Chromosome.fa.gz")
     
     
 def isheader(line):

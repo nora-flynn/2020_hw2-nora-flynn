4847db152993cb3f00bfde95ee6aefe2f7092369
nora-flynn <nflyn002@ucr.edu>
2020-11-06 12:08:49 -0800

    final for sevenless

--- sevenless.py
+++ sevenless.py
@@ -1,16 +1,23 @@
 #How to use range to get numbers from 0-99
+print("This is a range from 0-99")
 print(list(range(100)))
+print("-----")
 
 
 #First I made a range that counts by 7
 problem1 = list(range(100))
+print("This range counts by 7")
 
 for i in problem1:
     if (i%7==0):
         print(i)
         
+print("-----")
+        
         
 #Then I made the opposite (a sevenless range)
+print("This range is sevenless")
 for i in problem1:
     if (i%7!=0):
         print(i)
+print("-----")
